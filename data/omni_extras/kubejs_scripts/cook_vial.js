600b6db522cdad6fbb30f43e756cf91784dbede7
QuillLovesIT <shaquillerules@gmail.com>
2025-10-21 22:22:13 +0700

    eye guy default texture change ig

--- data/omni_extras/kubejs_scripts/cook_vial.js
+++
@@ -1,5 +0,0 @@
-ServerEvents.recipes(event => {
-  event.smelting('omni_extras:opticoid_vial', 'omni_extras:filled_vial')
-   .xp(1.0)        // sets XP (float)
-   .cookingTime(300); // sets smelting time in ticks (300 = 15s)
-})
