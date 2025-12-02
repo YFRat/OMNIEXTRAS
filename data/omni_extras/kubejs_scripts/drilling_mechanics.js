bafc8337dbe402814999d2c2a02087c59bf5bfae
QuillLovesIT <shaquillerules@gmail.com>
2025-12-02 22:17:22 +0700

    why did bro have 3 if statements..

--- data/omni_extras/kubejs_scripts/drilling_mechanics.js
+++ data/omni_extras/kubejs_scripts/drilling_mechanics.js
@@ -5,8 +5,6 @@ BlockEvents.broken(event => {
 
     if (isDrillo && event.block.hasTag('minecraft:mineable/pickaxe')) {
         if (limit === 300) return
-            if (event.block.hasTag('minecraft:mineable/pickaxe')) {
             palladium.scoreboard.addScore(player, 'Talpaedan.DigMeter', 2);
-            }  
         } 
 });
