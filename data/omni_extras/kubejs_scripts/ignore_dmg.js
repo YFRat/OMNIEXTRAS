2864add5fafbd9b9cf80746361a2ff307966fec4
QuillLovesIT <shaquillerules@gmail.com>
2025-12-04 15:04:30 +0700

    dont worry about golden hazard

--- data/omni_extras/kubejs_scripts/ignore_dmg.js
+++ data/omni_extras/kubejs_scripts/ignore_dmg.js
@@ -1,12 +1,12 @@
 EntityEvents.hurt((event) => {
     if (event.source == 'DamageSource (thrown)' && event.entity.isPlayer() && abilityUtil.isEnabled(event.entity, 'omni_extras:opticoid', 'opticoid_loop')) {
-        event.cancel(); 
+        event.cancel();
     }
 });
 
 EntityEvents.hurt((event) => {
     if (event.source == 'DamageSource (thrown)' && event.entity.isPlayer() && abilityUtil.isEnabled(event.entity, 'omni_extras:perkgourmand', 'gourmand_loop')) {
-        event.cancel(); 
+        event.cancel();
     }
 });
 
@@ -15,27 +15,27 @@ EntityEvents.hurt((event) => {
         if (event.entity.fallDistance > 15) {
             palladium.superpowers.addSuperpower(event.entity, "omni_extras:not_aliens/flatten_modified");
         }
-        event.cancel(); 
+        event.cancel();
     }
 });
 
 EntityEvents.hurt((event) => {
     if (event.source == 'DamageSource (thrown)' && event.entity.isPlayer() && abilityUtil.isEnabled(event.entity, 'omni_extras:murkgourmand', 'gourmand_loop')) {
-        event.cancel(); 
+        event.cancel();
     }
 });
 
 EntityEvents.hurt((event) => {
     if (event.source == 'DamageSource (fall)' && event.entity.isPlayer() && abilityUtil.isEnabled(event.entity, 'omni_extras:opticoid', 'opticoid_loop')) {
         if (event.entity.fallDistance < 14) {
-            event.cancel(); 
+            event.cancel();
         }
     }
 });
 
 EntityEvents.hurt((event) => {
     if (event.source == 'DamageSource (inWall)' && event.entity.isPlayer() && abilityUtil.isEnabled(event.entity, 'omni_extras:talpaedan', 'talpaedan_loop')) {
-            event.cancel(); 
+        event.cancel();
     }
 });
 /* EntityEvents.hurt((event) => {
@@ -44,4 +44,4 @@ EntityEvents.hurt((event) => {
 
     entity.tell(Text.of(dmgtype))
 });
-*/ 
+*/
